27ddc8adbb38bd623a9eca5f41aef8a961c49234
Masud <masud.petronia@gmail.com>
2021-12-22 11:48:12 +0000

    Step 2 complete. Tested by running the container (run_docker.sh) and making prediction (make_predictions.sh)

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,13 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=predicthouseprice .
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+# The container and host ports are in the Dockerfile and make_prediction.sh
+docker run -p 8000:80 predicthouseprice
